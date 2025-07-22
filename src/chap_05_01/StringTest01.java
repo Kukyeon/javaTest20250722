60557bc450000363cbfd9a7058ee44427bab3e83
Kukyeon <rnrdus2000@naver.com>
2025-07-22 10:22:19 +0900

    java20250722

---
+++ src/chap_05_01/StringTest01.java
@@ -0,0 +1,21 @@
+package chap_05_01;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch1 = 'k';
+		char ch2 = 'o';
+		char ch3 = 'r';
+		
+		char[] chArr = {'k','o','r'};
+		System.out.println(chArr);
+		
+		// 방식은 여러가지 
+		
+		String str1 = "kor";
+		
+	}
+
+}
